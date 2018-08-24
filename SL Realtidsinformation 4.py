5ef3dbfd1ffae4a1f6b5bae9f625d8566c7cc48c
naitmare01 <david.berndtsson@gmail.com>
2018-08-24 21:09:06 +0200

    new file:   SL Realtidsinformation 4.py

---
+++ SL Realtidsinformation 4.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import requests
+import json
+import datetime
+
+url = "https://api.sl.se/api2/realtimedeparturesV4.json"
+key = "05261ccaad7347a18a8960a4f57b4b91"
+querystring = {"key":"05261ccaad7347a18a8960a4f57b4b91","timeWindow":60,"siteid":"9509","bus":"false","metro":"false","tram":["false","false"]}
+
+headers = {
+    'Cache-Control': "no-cache",
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+jsonresponse = json.loads(response.text)
+
+for n in jsonresponse["ResponseData"]["Trains"]:
+    if n["LineNumber"] == "40" and n["Destination"] == "Uppsala C":
+        ExpectedDateTime = n["ExpectedDateTime"]
+        TimeTabledDateTime = n["TimeTabledDateTime"]
+        Destination = n["Destination"]
+
+        if ExpectedDateTime == TimeTabledDateTime:
+            onTime = "Yes"
+        else:
+            onTime = "Yes"
+
+        result = {'TrainOnTime': onTime,'TimeTabledDateTime': TimeTabledDateTime, 'ExpectedDepartureTime': ExpectedDateTime, 'Destination': Destination}
+        print(result)
