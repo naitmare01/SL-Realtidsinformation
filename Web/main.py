c04f2ebef7a42f81dafd27699f4e2ab279a10898
naitmare01 <david.berndtsson@gmail.com>
2018-08-27 07:43:10 +0200

    new file:   Web/Dockerfile 	new file:   Web/SolnaAndUppsalaOnTime.pyc 	modified:   Web/__pycache__/main.cpython-37.pyc 	modified:   Web/main.py 	modified:   Web/main.pyc 	new file:   Web/requirements.txt 	deleted:    Web/templates/hello.html 	deleted:    Web/templates/uppsala.html

--- Web/main.py
+++ Web/main.py
@@ -1,26 +1,14 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request
 from SolnaAndUppsalaOnTime import apiCall, clearJsonData
+import os, uuid
 app = Flask(__name__, static_folder='static')
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
 
 @app.route('/')
 def index():
-    return 'Index Page'
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
+    hostname = os.uname()[1]
+    randomid = uuid.uuid4()
+    return 'Container Hostname: ' + hostname + ' , ' + 'UUID: ' + str(randomid) + '\n'
 
 @app.route('/trains')
 def show_trains():
@@ -35,3 +23,6 @@ def show_trains():
     data = cleanedJsonSolna + cleanedJsonUppsala
 
     return render_template('trains.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True,port=80,host='0.0.0.0')
